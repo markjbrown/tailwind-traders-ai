df77f236da0f642170e462fe8550940046d4b2f5
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-09-24 09:28:08 -0500

    Adding AWS and Google TF project stubs

---
+++ terraform/aws/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  cloud {
+    organization = "tailwind-traders"
+    workspaces {
+      name = "tailwind-traders-aws"
+    }
+  }
+}
+
+provider "aws" {}
