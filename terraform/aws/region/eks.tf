43686dc44ddf35435c7f832cd45f227c70e731c2
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-10-11 14:08:30 -0500

    Creating EKS

--- terraform/aws/region/eks.tf
+++ terraform/aws/region/eks.tf
@@ -27,3 +27,40 @@ resource "aws_iam_role" "eks_cluster_role" {
 EOF
 }
 
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+data "tls_certificate" "app_eks" {
+  url = aws_eks_cluster.app_eks.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "app_eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = data.tls_certificate.app_eks.certificates.*.sha1_fingerprint
+  url             = data.tls_certificate.app_eks.url
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "${aws_eks_cluster.app_eks.identity[0].oidc[0].issuer}/:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.app_eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "app_eks" {
+  name               = "app_eks"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
