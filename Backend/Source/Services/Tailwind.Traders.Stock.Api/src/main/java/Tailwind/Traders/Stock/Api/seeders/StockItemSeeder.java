83a169154e779a16f0819ed6f18dee8a3446b46f
faiz <faiz>
2022-10-17 20:46:03 +0530

    added

--- Backend/Source/Services/Tailwind.Traders.Stock.Api/src/main/java/Tailwind/Traders/Stock/Api/seeders/StockItemSeeder.java
+++ Backend/Source/Services/Tailwind.Traders.Stock.Api/src/main/java/Tailwind/Traders/Stock/Api/seeders/StockItemSeeder.java
@@ -36,19 +36,22 @@ public class StockItemSeeder {
 			System.out.println("No Database Selected");
 			System.exit(1);
 		}
+		seed();
 	}
 
-	@EventListener
-	public void seed(ContextRefreshedEvent event) throws IOException {
+	public void seed() {
 		boolean alreadySeeded = stockItemRepository.count() > 0;
-
 		if (alreadySeeded) {
 			return;
 		}
-
-		BufferedReader reader = Files.newBufferedReader(Paths.get("setup/StockProduct.csv"), StandardCharsets.UTF_8);
-		List<StockProduct> allStock = new CsvToBeanBuilder<StockProduct>(reader).withType(StockProduct.class).build()
-				.parse();
+		BufferedReader reader;
+		List<StockProduct> allStock = null;
+		try {
+			reader = Files.newBufferedReader(Paths.get("setup/StockProduct.csv"), StandardCharsets.UTF_8);
+			allStock = new CsvToBeanBuilder<StockProduct>(reader).withType(StockProduct.class).build().parse();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		List<Integer> setted = new ArrayList<Integer>();
 
 		for (StockProduct stock : allStock) {
