c906613aac19831c4263387bf353cd71f314bfca
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-10-17 13:37:47 -0500

    Working on EKS ALB policies

--- terraform/aws/region/eks.tf
+++ terraform/aws/region/eks.tf
@@ -46,6 +46,76 @@ module "eks" {
   }
 }
 
+module "lb_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name = "${local.resource_prefix}-APP-lb-role"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "service_account" {
+  metadata {
+    name = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      app.kubernetes.io/name = "aws-load-balancer-controller"
+      app.kubernetes.io/component = "controller"
+    }
+    annotations = {
+      eks.amazonaws.com/role-arn = module.lb_role.role_arn
+      eks.amazonaws.com/sts-regional-endpoints = "true"
+    }
+  }
+}
+
+resource "helm_release" "lb" {
+    name = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    chart = "aws-load-balancer-controller"
+    repository = "https://aws.github.io/eks-charts"
+    version = "2.4.0"
+    depends_on = [
+        kubernetes_service_account.service_account
+    ]
+
+  set {
+    name  = "region"
+    value = "us-east-1"
+  }
+
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/amazon/aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_id
+  }
+}
+
 resource "aws_iam_group" "eks_admin" {
   name = "eks-admin"
 }
--- terraform/aws/region/providers.tf
+++ terraform/aws/region/providers.tf
@@ -7,3 +7,15 @@ provider "kubernetes" {
     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
   }
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+    }
+  }
+}
