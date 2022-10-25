5ec0202d749718d26947c31313c22d58c0ac7339
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-10-25 09:40:30 -0500

    Making bucket name lowercase

--- terraform/aws/global/s3.tf
+++ terraform/aws/global/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "image_storage" {
-  bucket = join("-", [local.resource_prefix, "IMG", "s3"])
+  bucket = lower(join("-", [local.resource_prefix, "IMG", "s3"]))
 }
 
 resource "aws_s3_bucket_acl" "image_storage" {
