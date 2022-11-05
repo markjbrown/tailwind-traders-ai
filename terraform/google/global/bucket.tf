24ba1ea288df96fdecdfa60eb97cf8dc2c6fb2de
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-11-05 16:36:06 -0500

    Enabling public access on google bucket for images

--- terraform/google/global/bucket.tf
+++ terraform/google/global/bucket.tf
@@ -20,3 +20,11 @@ resource "google_storage_default_object_access_control" "public_read" {
   role   = "READER"
   entity = "allUsers"
 }
+
+resource "google_storage_bucket_iam" "public_read" {
+  bucket = google_storage_bucket.image_store.name
+  role   = "roles/storage.objectViewer"
+  members = [
+    "allUsers",
+  ]
+}
