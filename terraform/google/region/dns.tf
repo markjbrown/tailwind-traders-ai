f3998e396e3df8005c1cab8143b3ff9ee4ae9a53
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-11-25 13:40:41 -0600

    Pointing at correct gke entry in DNS

--- terraform/google/region/dns.tf
+++ terraform/google/region/dns.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_record" "eks" {
+resource "aws_route53_record" "gke" {
   zone_id = var.zone_id
-  name    = "eks.${var.domain_name}"
+  name    = "gke.${var.domain_name}"
   type    = "A"
   ttl     = "300"
   records = [google_compute_address.ip.address]
