1f1d5945d4a76acd729d245a47a59c6d2d4839eb
scottsolliance <scott@solliance.net>
2022-12-05 07:23:01 -0800

    missed a file with previous checkin on main

--- Backend/Source/Services/Tailwind.Traders.Product.Api/Dtos/ProductBrandDto.cs
+++ Backend/Source/Services/Tailwind.Traders.Product.Api/Dtos/ProductBrandDto.cs
@@ -1,9 +1,13 @@
-﻿namespace Tailwind.Traders.Product.Api.Dtos
+﻿using Newtonsoft.Json;
+
+namespace Tailwind.Traders.Product.Api.Dtos
 {
     public class ProductBrandDto
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
 
+        [JsonProperty("name")]
         public string Name { get; set; }
     }
 }
