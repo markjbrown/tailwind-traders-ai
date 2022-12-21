0657d8f336e5e3d851362284833afdd002574c3c
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-12-21 15:56:39 -0600

    Updating Dockerfile definitions to use .NET 6.0 SDK and Runtime base images

--- terraform/google/region/gke.tf
+++ terraform/google/region/gke.tf
@@ -50,7 +50,7 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "1.10.0"
+  version    = "v1.10.0"
   namespace  = kubernetes_namespace.cert_manager.metadata[0].name
 
 
